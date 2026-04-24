072d20199f76554f65bd9c5979d98fb70bc190c0
Vsd1208 <saidhirajvandanapu@gmail.com>
2026-04-24 21:09:24 +0530

    Clean commit without secrets

---
+++ call.js
@@ -0,0 +1,12 @@
+const twilio = require("twilio");
+
+const client = twilio(accountSid, authToken);
+
+client.calls
+  .create({
+    url: "https://bobcat-relock-imprison.ngrok-free.dev/voice/incoming",
+    to: "+918897587467",
+    from: "+19062993655" // your Twilio number
+  })
+  .then(call => console.log("Call started:", call.sid))
+  .catch(err => console.error(err));
